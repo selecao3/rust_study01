047f254bcafb43a0233fc17241217cae77895b83
kazuha <mami.tomoe4@gmail.com>
2018-05-09 09:35:08 +0900

    Studying rust by rocket

---
+++ src/main.rs
@@ -0,0 +1,122 @@
+#![feature(plugin, decl_macro, custom_derive, const_fn)]
+#![plugin(rocket_codegen)]
+
+extern crate rocket;
+#[macro_use] extern crate diesel;
+#[macro_use] extern crate serde_derive;
+extern crate rocket_contrib;
+
+mod static_files;
+mod task;
+mod db;
+#[cfg(test)] mod tests;
+
+//同ディレクトリ内にあるフォルダはpubをつけなくて良い
+
+use rocket::Rocket;
+use rocket::request::{Form, FlashMessage};
+use rocket::response::{Flash, Redirect};
+use rocket_contrib::Template;
+
+use task::{Task, Todo};
+
+#[derive(Debug, Serialize)]
+//Serialize: データベースに配列データをそのまま保存したい時などに使う
+//アトリビュートについて：　アトリビュートはstructだけに適用されるものではない。
+// 例えば、ある関数の上に#[test]というアトリビュートが付くと、その関数はテストを実行した際に自動的に実行されるテスト関数になる。
+struct Context<'a, 'b>{
+    msg: Option<(&'a str, &'b str)>,
+    tasks: Vec<Task>
+}
+//'aでライフタイムを無視する。ざっくり言うとどこからでも引数が受け取れる
+
+
+impl<'a, 'b> Context<'a, 'b> {
+    //struct Contextに対するimpl
+    pub fn err(conn: &db::Conn, msg: &'a str) -> Context<'static, 'a> {
+        Context{msg: Some(("error", msg)), tasks: Task::all(conn)}
+    }
+
+    pub fn raw(conn: &db::Conn, msg: Option<(&'a str, &'b str)>) -> Context<'a, 'b> {
+        Context{msg: msg, tasks: Task::all(conn)}
+    }
+}
+
+#[post("/", data = "<todo_form>")]
+//アトリビュートpostにより、new関数はpost時に実行される関数に。
+fn new(todo_form: Form<Todo>, conn: db::Conn) -> Flash<Redirect> {
+    //rustではわざわざ返り値の型を書かなければならない。何故ならコンパイルエラー時に問題を分かりやすくするため.
+    /*
+    ・返り値について
+    ややこしいけど、この場合のFlash<Redirect>という型で考える.
+    まずFlashもRedirectも構造体である。
+    下のif文を通ると漏れなくFlash::errorを通る。（定義を飛ぶと書いてあるけど）紆余曲折を経て、
+    構造体Flashはパラメータを得る（name, message, consumed, innerの４つ ）
+    次に構造体Redirectにパラメータが渡される。
+
+    ざっくり言うと、裏ではstruct, impl, fnの三つが動いて複雑な構造を作っている。
+    それに合わせて、返り値も決まった形を返す。
+
+    */
+    let todo = todo_form.into_inner();
+    if todo.description.is_empty() {
+        Flash::error(Redirect::to("/"), "Description cannot be empty.")
+        //何も書かずにpostした時のエラー文
+    } else if Task::insert(todo, &conn) {
+        Flash::success(Redirect::to("/"), "Todo successfully added.")
+    } else {
+        Flash::error(Redirect::to("/"), "Whoops! The server failed.")
+    }
+}
+/*
+new関数でやってること
+・
+
+*/
+
+#[put("/<id>")]
+fn toggle(id: i32, conn: db::Conn) -> Result<Redirect, Template> {
+    if Task::toggle_with_id(id, &conn) {
+        Ok(Redirect::to("/"))
+    } else {
+        Err(Template::render("index", &Context::err(&conn, "Couldn't toggle task.")))
+    }
+}
+
+#[delete("/<id>")]
+fn delete(id: i32, conn: db::Conn) -> Result<Flash<Redirect>, Template> {
+    if Task::delete_with_id(id, &conn) {
+        Ok(Flash::success(Redirect::to("/"), "Todo was deleted."))
+    } else {
+        Err(Template::render("index", &Context::err(&conn, "Couldn't delete task.")))
+    }
+}
+
+#[get("/")]
+fn index(msg: Option<FlashMessage>, conn: db::Conn) -> Template {
+    Template::render("index", &match msg {
+        Some(ref msg) => Context::raw(&conn, Some((msg.name(), msg.msg()))),
+        None => Context::raw(&conn, None),
+    })
+}
+
+fn rocket() -> (Rocket, Option<db::Conn>) {
+    let pool = db::init_pool();
+    let conn = if cfg!(test) {
+        Some(db::Conn(pool.get().expect("database connection for testing")))
+    } else {
+        None
+    };
+
+    let rocket = rocket::ignite()
+        .manage(pool)
+        .mount("/", routes![index, static_files::all])
+        .mount("/todo/", routes![new, toggle, delete])
+        .attach(Template::fairing());
+
+    (rocket, conn)
+}
+
+fn main() {
+    rocket().0.launch();
+}
